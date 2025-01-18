a343c59085da7b36cf0b5da94688e18a07ade430
Ikbal Hossain <indikbal@gmail.com>
2025-01-18 17:52:30 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,59 @@
+
+$('#creator_client').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+
+$('#our_client').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    navText: [
+    '<i class="fa-solid fa-arrow-left-long"></i>',
+    '<i class="fa-solid fa-arrow-right-long"></i>'
+],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+
+
+const navBar = document.querySelector("nav"),
+    menuBtns = document.querySelectorAll(".menu-icon"),
+    overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
